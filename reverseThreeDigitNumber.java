9b9f0e4830431eb1739156640ba66c92a2d2bfa2
Lev <levdjepko@gmail.com>
2022-04-14 10:30:37 -0700

    Create reverseThreeDigitNumber.java

---
+++ reverseThreeDigitNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int initial = scanner.nextInt();
+        int single = initial % 10; // last digit 1
+        int decimal = (initial % 100 - single) / 10; // second digit 2
+        int hundreds = (initial % 1000 - decimal) / 100; // first digit 3
+        // now turn them around:
+        int newNumber = single*100 + decimal*10 + hundreds;
+        System.out.println(newNumber);
+    }
+}
