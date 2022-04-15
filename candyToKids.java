12e7888ece489dcfd8deb16873207fc6c1fc9861
Lev <levdjepko@gmail.com>
2022-04-15 11:02:33 -0700

    Create candyToKids.java
    every kid gets candy, they all have ratings. The higher the rating, the more candy

---
+++ candyToKids.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Scratch {
+    public static void main(String[] args) {
+        int[] A = {-1, -2, 2, 2, 3};
+        Arrays.sort(A);
+        //System.out.println(Arrays.toString(A));
+        //System.out.println(A.length);
+        int smallest = A[0];
+
+        int totalCandy = 0;
+        int increment = 1;
+        //then go over every element of the array and give candy
+        for(int i = 0; i < A.length; i++){
+            //smallest element gets one candy
+            if(A[i] > smallest){ // this element is bigger than before
+                increment++;
+            }
+            totalCandy += increment;
+            smallest  = A[i]; // now we have a smallest as the current so we can compare it in the next step
+        };
+        System.out.println(totalCandy);
+    }
+}
