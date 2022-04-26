9fe7d60903b268c625235d5f9d98e9e291195762
Lev <levdjepko@gmail.com>
2022-04-26 10:25:10 -0700

    Update ticTacToe.java

--- ticTacToe.java
+++ ticTacToe.java
@@ -1,9 +1,26 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("X O X");
-        System.out.println("X O X");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter cells: ");
+        String input = scanner.next();
+        //xo_xo_xo_
+        char[] cells = new char[9];
+        //(_ _ _ _ _ _ _ _ _)
+        for (int i = 0; i < input.length(); i++) {
+            cells[i] = input.charAt(i);
+        }
+        System.out.println("---------");
+        System.out.print("| " + cells[0] + " " + cells[1] + " " + cells[2]+ " ");
+        System.out.println("|");
+        System.out.print("| " + cells[3] + " " + cells[4] + " " + cells[5]+ " ");
+        System.out.println("|");
+        System.out.print("| " + cells[6] + " " + cells[7] + " " + cells[8]+ " ");
+        System.out.println("|");
+        System.out.println("---------");
+
     }
 }
