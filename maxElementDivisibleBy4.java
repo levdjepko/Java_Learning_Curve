c5ae30bceb61425a2773e4320fb182e71069988c
Lev <levdjepko@gmail.com>
2022-04-27 11:33:36 -0700

    Create maxElementDivisibleBy4.java
    find a biggest element in unsorted sequence, that is divisible by 4

---
+++ maxElementDivisibleBy4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // You are given a sequence of natural numbers,
+        // not exceeding 30000. Find the maximum element
+        // divisible by 4. As input, the program gets the
+        // number of elements in the sequence, and then
+        // the elements themselves. In the sequence, there
+        // is always an element divisible by 4. The number
+        // of elements does not exceed 1000.
+        // The program should print a single number:
+        // the maximum element of the sequence divisible by 4.
+        int totalNumbers = scanner.nextInt();
+        int currentElement = 0;
+        int max = -1;
+        while(currentElement < totalNumbers){
+            int element = scanner.nextInt();
+            if(element % 4 == 0 && element >= max) {
+                max = element;
+            }
+            currentElement++;
+        }
+        System.out.println(max);
+    }
+}
