42fa792f378d8ccee6befee421ba5dc712bd1c85
Lev <levdjepko@gmail.com>
2022-05-03 09:35:09 -0700

    Create rotate2DArray90Degrees.java

---
+++ rotate2DArray90Degrees.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // Given a rectangle array n×m in size.
+        // Rotate it by 90 degrees clockwise,
+        // by recording the result into the
+        // new array m×n in size.
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] initialArray = new int[n][m]; // 3 x 4
+        int[][] rotatedArray = new int[m][n]; // 4 x 3
+
+        //fill the initial 2D array:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                initialArray[i][j] = scanner.nextInt();
+            }
+        }
+        // now we iterate over the array and fill the new one, but rotated:
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < m; j++) {
+                rotatedArray[j][n - i - 1] = initialArray[i][j];// < - rotation is here
+            }
+        }
+        // Next is just to print out all the elements:
+        for (int i = 0; i < rotatedArray.length; i++) {
+            for (int j = 0; j < rotatedArray[i].length; j++) {
+                System.out.print(Integer.toString(rotatedArray[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
