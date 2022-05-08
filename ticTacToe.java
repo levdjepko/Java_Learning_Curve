44ef306c56352007c647491b814cc59e6d5ab6ff
Lev <levdjepko@gmail.com>
2022-05-08 10:05:38 -0700

    Update ticTacToe.java
    Added the logic to let the player make a move with 'X'

--- ticTacToe.java
+++ ticTacToe.java
@@ -19,19 +19,44 @@ public class Main {
             }
         }
         //this just prints out the state of the field:
-        System.out.println("---------");
-        System.out.print("| " + gameState[0][0] + " " + gameState[0][1] + " " + gameState[0][2]+ " ");
-        System.out.println("|");
-        System.out.print("| " + gameState[1][0] + " " + gameState[1][1] + " " + gameState[1][2]+ " ");
-        System.out.println("|");
-        System.out.print("| " + gameState[2][0] + " " + gameState[2][1] + " " + gameState[2][2]+ " ");
-        System.out.println("|");
-        System.out.println("---------");
-        //variables to keep the state
+        outputGameState(gameState);
+
         int countX = 0;
         int countO = 0;
         int countEmpty = 0;
 
+        //expecting the coordinates from 1 to 3
+        System.out.println("Enter the coordinates: ");
+        int inputRow = -1;
+        int inputColumn = -1;
+        boolean coordinatesCorrect = true;
+        boolean gameFinished = false;
+
+        while (!gameFinished) {
+            try {
+                inputRow = scanner.nextInt();
+                inputColumn = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+
+            if ((inputRow < 1 || inputRow > 3 || inputColumn < 1 || inputColumn > 3) && coordinatesCorrect) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+            //looks like it works, now check the cell
+            if (coordinatesCorrect && gameState[inputRow - 1][inputColumn - 1] != '_') {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            } else if (coordinatesCorrect) {
+                gameState[inputRow - 1][inputColumn - 1] = 'X';
+            }
+            if (coordinatesCorrect) {
+                outputGameState(gameState);
+                gameFinished = true;
+            }
+        }
         for (int i = 0; i < 9; i++) {
             if (input.charAt(i) == 'X') {
                 countX++;
@@ -44,34 +69,34 @@ public class Main {
         if (countX - countO > 1 || countO - countX > 1 ||
                 (
                         (input.charAt(0) == 'X' && input.charAt(1) == 'X' && input.charAt(2) == 'X' ||
-                input.charAt(3) == 'X' && input.charAt(4) == 'X' && input.charAt(5) == 'X' ||
-                input.charAt(6) == 'X' && input.charAt(7) == 'X' && input.charAt(8) == 'X' ||
-                input.charAt(0) == 'X' && input.charAt(3) == 'X' && input.charAt(6) == 'X' ||
-                input.charAt(1) == 'X' && input.charAt(4) == 'X' && input.charAt(7) == 'X' ||
-                input.charAt(2) == 'X' && input.charAt(5) == 'X' && input.charAt(8) == 'X' ||
-                input.charAt(0) == 'X' && input.charAt(4) == 'X' && input.charAt(8) == 'X' ||
-                input.charAt(2) == 'X' && input.charAt(4) == 'X' && input.charAt(6) == 'X' )
+                                input.charAt(3) == 'X' && input.charAt(4) == 'X' && input.charAt(5) == 'X' ||
+                                input.charAt(6) == 'X' && input.charAt(7) == 'X' && input.charAt(8) == 'X' ||
+                                input.charAt(0) == 'X' && input.charAt(3) == 'X' && input.charAt(6) == 'X' ||
+                                input.charAt(1) == 'X' && input.charAt(4) == 'X' && input.charAt(7) == 'X' ||
+                                input.charAt(2) == 'X' && input.charAt(5) == 'X' && input.charAt(8) == 'X' ||
+                                input.charAt(0) == 'X' && input.charAt(4) == 'X' && input.charAt(8) == 'X' ||
+                                input.charAt(2) == 'X' && input.charAt(4) == 'X' && input.charAt(6) == 'X')
                                 &&
-                (input.charAt(0) == 'O' && input.charAt(1) == 'O' && input.charAt(2) == 'O' ||
-                input.charAt(3) == 'O' && input.charAt(4) == 'O' && input.charAt(5) == 'O' ||
-                input.charAt(6) == 'O' && input.charAt(7) == 'O' && input.charAt(8) == 'O' ||
-                input.charAt(0) == 'O' && input.charAt(3) == 'O' && input.charAt(6) == 'O' ||
-                input.charAt(1) == 'O' && input.charAt(4) == 'O' && input.charAt(7) == 'O' ||
-                input.charAt(2) == 'O' && input.charAt(5) == 'O' && input.charAt(8) == 'O' ||
-                input.charAt(0) == 'O' && input.charAt(4) == 'O' && input.charAt(8) == 'O' ||
-                input.charAt(2) == 'O' && input.charAt(4) == 'O' && input.charAt(6) == 'O' )
+                                (input.charAt(0) == 'O' && input.charAt(1) == 'O' && input.charAt(2) == 'O' ||
+                                        input.charAt(3) == 'O' && input.charAt(4) == 'O' && input.charAt(5) == 'O' ||
+                                        input.charAt(6) == 'O' && input.charAt(7) == 'O' && input.charAt(8) == 'O' ||
+                                        input.charAt(0) == 'O' && input.charAt(3) == 'O' && input.charAt(6) == 'O' ||
+                                        input.charAt(1) == 'O' && input.charAt(4) == 'O' && input.charAt(7) == 'O' ||
+                                        input.charAt(2) == 'O' && input.charAt(5) == 'O' && input.charAt(8) == 'O' ||
+                                        input.charAt(0) == 'O' && input.charAt(4) == 'O' && input.charAt(8) == 'O' ||
+                                        input.charAt(2) == 'O' && input.charAt(4) == 'O' && input.charAt(6) == 'O')
                 )
         ) {
-            System.out.println("Impossible");
-        }  else if (input.charAt(0) == 'X' && input.charAt(1) == 'X' && input.charAt(2) == 'X' ||
+            //System.out.println("Impossible");
+        } else if (input.charAt(0) == 'X' && input.charAt(1) == 'X' && input.charAt(2) == 'X' ||
                 input.charAt(3) == 'X' && input.charAt(4) == 'X' && input.charAt(5) == 'X' ||
                 input.charAt(6) == 'X' && input.charAt(7) == 'X' && input.charAt(8) == 'X' ||
                 input.charAt(0) == 'X' && input.charAt(3) == 'X' && input.charAt(6) == 'X' ||
                 input.charAt(1) == 'X' && input.charAt(4) == 'X' && input.charAt(7) == 'X' ||
                 input.charAt(2) == 'X' && input.charAt(5) == 'X' && input.charAt(8) == 'X' ||
                 input.charAt(0) == 'X' && input.charAt(4) == 'X' && input.charAt(8) == 'X' ||
-                input.charAt(2) == 'X' && input.charAt(4) == 'X' && input.charAt(6) == 'X' ) {
-            System.out.println("X wins");
+                input.charAt(2) == 'X' && input.charAt(4) == 'X' && input.charAt(6) == 'X') {
+            //System.out.println("X wins");
         } else if (input.charAt(0) == 'O' && input.charAt(1) == 'O' && input.charAt(2) == 'O' ||
                 input.charAt(3) == 'O' && input.charAt(4) == 'O' && input.charAt(5) == 'O' ||
                 input.charAt(6) == 'O' && input.charAt(7) == 'O' && input.charAt(8) == 'O' ||
@@ -79,12 +104,23 @@ public class Main {
                 input.charAt(1) == 'O' && input.charAt(4) == 'O' && input.charAt(7) == 'O' ||
                 input.charAt(2) == 'O' && input.charAt(5) == 'O' && input.charAt(8) == 'O' ||
                 input.charAt(0) == 'O' && input.charAt(4) == 'O' && input.charAt(8) == 'O' ||
-                input.charAt(2) == 'O' && input.charAt(4) == 'O' && input.charAt(6) == 'O' ) {
-            System.out.println("O wins");
+                input.charAt(2) == 'O' && input.charAt(4) == 'O' && input.charAt(6) == 'O') {
+            //System.out.println("O wins");
         } else if (countEmpty > 0) {
-            System.out.println("Game not finished");
+            //System.out.println("Game not finished");
         } else if (countEmpty == 0) {
-            System.out.println("Draw");
+            //System.out.println("Draw");
         }
+    }
+
+    private static void outputGameState(char[][] gameState) {
+        System.out.println("---------");
+        System.out.print("| " + gameState[0][0] + " " + gameState[0][1] + " " + gameState[0][2] + " ");
+        System.out.println("|");
+        System.out.print("| " + gameState[1][0] + " " + gameState[1][1] + " " + gameState[1][2] + " ");
+        System.out.println("|");
+        System.out.print("| " + gameState[2][0] + " " + gameState[2][1] + " " + gameState[2][2] + " ");
+        System.out.println("|");
+        System.out.println("---------");
     }
 }
