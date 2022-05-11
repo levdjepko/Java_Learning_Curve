84efd4f0e85b41b403384a30460b40b9eb6d42b8
Lev <levdjepko@gmail.com>
2022-05-11 09:47:21 -0700

    Create javaSimpleCalculator.java

---
+++ javaSimpleCalculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        long firstNumber = Long.parseLong(scanner.next());
+        String operation = scanner.next();
+        long secondNumber = Long.parseLong(scanner.next());
+
+        switch (operation) {
+            case "+": {
+                System.out.println(firstNumber + secondNumber);
+                break;
+            }
+            case "-": {
+                System.out.println(firstNumber - secondNumber);
+                break;
+            }
+            case "/": {
+                if (secondNumber != 0) {
+                    System.out.println(firstNumber / secondNumber);
+                    break;
+                }
+                else {
+                    System.out.println("Division by 0!");
+                    break;
+                }
+            }
+            case "*": {
+                System.out.println(firstNumber * secondNumber);
+                break;
+            }
+            default: {
+                System.out.println("Unknown operator");
+                break;
+            }
+        }
+    }
+}
