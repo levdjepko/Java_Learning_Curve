70b58a515bb4cb930f8c3b3dd8119f92be747d58
Lev <levdjepko@gmail.com>
2022-05-13 11:17:55 -0700

    Update ticTacToe.java

--- ticTacToe.java
+++ ticTacToe.java
@@ -6,19 +6,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter cells: ");
-        String input = scanner.next();
-        //xo_xo_xo_
-        //(_ _ _ _ _ _ _ _ _)
-        // now we need a 2D array to get the state of this game:
+        //output the initial state of the field (empty):
         char[][] gameState = new char[3][3];
-        //fill this array with the values that the user input:
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                gameState[i][j] = input.charAt(i * 3 + j);
-            }
-        }
-        //this just prints out the state of the field:
+        Arrays.fill(gameState[0], '_');
+        Arrays.fill(gameState[1], '_');
+        Arrays.fill(gameState[2], '_');
         outputGameState(gameState);
 
         int countX = 0;
@@ -29,6 +21,7 @@ public class Main {
         System.out.println("Enter the coordinates: ");
         int inputRow = -1;
         int inputColumn = -1;
+        int currentPlayer = 1;
         boolean coordinatesCorrect = true;
         boolean gameFinished = false;
 
@@ -50,68 +43,64 @@ public class Main {
                 System.out.println("This cell is occupied! Choose another one!");
                 continue;
             } else if (coordinatesCorrect) {
-                gameState[inputRow - 1][inputColumn - 1] = 'X';
+                if (currentPlayer == 1) {
+                    gameState[inputRow - 1][inputColumn - 1] = 'X';
+                    currentPlayer = 2;
+                }
+                else {
+                    gameState[inputRow - 1][inputColumn - 1] = 'O';
+                    currentPlayer = 1;
+                }
             }
             if (coordinatesCorrect) {
                 outputGameState(gameState);
+            }
+            // here comes the check of the game state (x wins, o wins, draw):
+            countX = 0;
+            countO = 0;
+            countEmpty = 0;
+            for (int row = 0; row < 3; row++) {
+                for (int column = 0; column < 3; column++) {
+                    if (gameState[row][column] == 'X') {
+                        countX++;
+                    } else if (gameState[row][column] == 'O') {
+                        countO++;
+                    } else {
+                        countEmpty++;
+                    }
+                }
+            }
+            if (gameState[0][0] == 'X' && gameState[0][1] == 'X' && gameState[0][2] == 'X' ||
+                    gameState[1][0] == 'X' && gameState[1][1] == 'X' && gameState[1][2] == 'X' ||
+                    gameState[2][0] == 'X' && gameState[2][1] == 'X' && gameState[2][2] == 'X' ||
+                    gameState[0][0] == 'X' && gameState[1][0] == 'X' && gameState[2][0] == 'X' ||
+                    gameState[0][1] == 'X' && gameState[1][1] == 'X' && gameState[2][1] == 'X' ||
+                    gameState[0][2] == 'X' && gameState[1][2] == 'X' && gameState[2][2] == 'X' ||
+                    gameState[0][0] == 'X' && gameState[1][1] == 'X' && gameState[2][2] == 'X' ||
+                    gameState[2][0] == 'X' && gameState[1][1] == 'X' && gameState[0][2] == 'X') {
+                                System.out.println("X wins");
+                                gameFinished = true;
+            }
+            else if (gameState[0][0] == 'O' && gameState[0][1] == 'O' && gameState[0][2] == 'O' ||
+                    gameState[1][0] == 'O' && gameState[1][1] == 'O' && gameState[1][2] == 'O' ||
+                    gameState[2][0] == 'O' && gameState[2][1] == 'O' && gameState[2][2] == 'O' ||
+                    gameState[0][0] == 'O' && gameState[1][0] == 'O' && gameState[2][0] == 'O' ||
+                    gameState[0][1] == 'O' && gameState[1][1] == 'O' && gameState[2][1] == 'O' ||
+                    gameState[0][2] == 'O' && gameState[1][2] == 'O' && gameState[2][2] == 'O' ||
+                    gameState[0][0] == 'O' && gameState[1][1] == 'O' && gameState[2][2] == 'O' ||
+                    gameState[2][0] == 'O' && gameState[1][1] == 'O' && gameState[0][2] == 'O') {
+                                System.out.println("O wins");
+                                gameFinished = true;
+            }
+            else if (countEmpty == 0) {
+                System.out.println("Draw");
                 gameFinished = true;
             }
         }
-        for (int i = 0; i < 9; i++) {
-            if (input.charAt(i) == 'X') {
-                countX++;
-            } else if (input.charAt(i) == 'O') {
-                countO++;
-            } else {
-                countEmpty++;
-            }
+
         }
-        if (countX - countO > 1 || countO - countX > 1 ||
-                (
-                        (input.charAt(0) == 'X' && input.charAt(1) == 'X' && input.charAt(2) == 'X' ||
-                                input.charAt(3) == 'X' && input.charAt(4) == 'X' && input.charAt(5) == 'X' ||
-                                input.charAt(6) == 'X' && input.charAt(7) == 'X' && input.charAt(8) == 'X' ||
-                                input.charAt(0) == 'X' && input.charAt(3) == 'X' && input.charAt(6) == 'X' ||
-                                input.charAt(1) == 'X' && input.charAt(4) == 'X' && input.charAt(7) == 'X' ||
-                                input.charAt(2) == 'X' && input.charAt(5) == 'X' && input.charAt(8) == 'X' ||
-                                input.charAt(0) == 'X' && input.charAt(4) == 'X' && input.charAt(8) == 'X' ||
-                                input.charAt(2) == 'X' && input.charAt(4) == 'X' && input.charAt(6) == 'X')
-                                &&
-                                (input.charAt(0) == 'O' && input.charAt(1) == 'O' && input.charAt(2) == 'O' ||
-                                        input.charAt(3) == 'O' && input.charAt(4) == 'O' && input.charAt(5) == 'O' ||
-                                        input.charAt(6) == 'O' && input.charAt(7) == 'O' && input.charAt(8) == 'O' ||
-                                        input.charAt(0) == 'O' && input.charAt(3) == 'O' && input.charAt(6) == 'O' ||
-                                        input.charAt(1) == 'O' && input.charAt(4) == 'O' && input.charAt(7) == 'O' ||
-                                        input.charAt(2) == 'O' && input.charAt(5) == 'O' && input.charAt(8) == 'O' ||
-                                        input.charAt(0) == 'O' && input.charAt(4) == 'O' && input.charAt(8) == 'O' ||
-                                        input.charAt(2) == 'O' && input.charAt(4) == 'O' && input.charAt(6) == 'O')
-                )
-        ) {
-            //System.out.println("Impossible");
-        } else if (input.charAt(0) == 'X' && input.charAt(1) == 'X' && input.charAt(2) == 'X' ||
-                input.charAt(3) == 'X' && input.charAt(4) == 'X' && input.charAt(5) == 'X' ||
-                input.charAt(6) == 'X' && input.charAt(7) == 'X' && input.charAt(8) == 'X' ||
-                input.charAt(0) == 'X' && input.charAt(3) == 'X' && input.charAt(6) == 'X' ||
-                input.charAt(1) == 'X' && input.charAt(4) == 'X' && input.charAt(7) == 'X' ||
-                input.charAt(2) == 'X' && input.charAt(5) == 'X' && input.charAt(8) == 'X' ||
-                input.charAt(0) == 'X' && input.charAt(4) == 'X' && input.charAt(8) == 'X' ||
-                input.charAt(2) == 'X' && input.charAt(4) == 'X' && input.charAt(6) == 'X') {
-            //System.out.println("X wins");
-        } else if (input.charAt(0) == 'O' && input.charAt(1) == 'O' && input.charAt(2) == 'O' ||
-                input.charAt(3) == 'O' && input.charAt(4) == 'O' && input.charAt(5) == 'O' ||
-                input.charAt(6) == 'O' && input.charAt(7) == 'O' && input.charAt(8) == 'O' ||
-                input.charAt(0) == 'O' && input.charAt(3) == 'O' && input.charAt(6) == 'O' ||
-                input.charAt(1) == 'O' && input.charAt(4) == 'O' && input.charAt(7) == 'O' ||
-                input.charAt(2) == 'O' && input.charAt(5) == 'O' && input.charAt(8) == 'O' ||
-                input.charAt(0) == 'O' && input.charAt(4) == 'O' && input.charAt(8) == 'O' ||
-                input.charAt(2) == 'O' && input.charAt(4) == 'O' && input.charAt(6) == 'O') {
-            //System.out.println("O wins");
-        } else if (countEmpty > 0) {
-            //System.out.println("Game not finished");
-        } else if (countEmpty == 0) {
-            //System.out.println("Draw");
-        }
-    }
+
+
 
     private static void outputGameState(char[][] gameState) {
         System.out.println("---------");
