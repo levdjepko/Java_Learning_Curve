08c8abaef4e152147e1dec1a928ad4097b9f938b
Lev <levdjepko@gmail.com>
2022-05-16 20:14:36 -0700

    Create convertAmPmTimeTo24Hour.java
    This converts and 12-hour formatted time to 24-hour("Military") format

---
+++ convertAmPmTimeTo24Hour.java
@@ -0,0 +1,25 @@
+private static String timeConversion(String input) {
+        int hour = Integer.parseInt(input.substring(0, 2));
+        String hourString = input.substring(0, 2);
+        String minutes = input.substring(3, 5);
+        String seconds = input.substring(6, 8);
+        String timeOfDay = input.substring(8, 10);
+
+        StringBuilder sb = new StringBuilder();
+        if (hour == 12 && timeOfDay.equals("PM")) {
+            sb.append(hour); // appends 12 like for PM time
+        } else if (hour == 12 && timeOfDay.equals("AM")) {
+            sb.append("00"); // the day begins with a zero time
+        } else if (timeOfDay.equals("PM")) {
+            int hourInPM = hour + 12;
+            sb.append(Integer.toString(hourInPM));
+        } else {
+            sb.append(hourString);
+        }
+        sb.append(":");
+        sb.append(minutes);
+        sb.append(":");
+        sb.append(seconds);
+        String result = sb.toString();
+        return result;
+    }
