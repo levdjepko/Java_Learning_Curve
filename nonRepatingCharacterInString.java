6533e8e7011dede33ce9d58af83e8a957e40675f
Lev <levdjepko@gmail.com>
2022-05-17 19:47:33 -0700

    Update nonRepatingCharacterInString.java

--- nonRepatingCharacterInString.java
+++ nonRepatingCharacterInString.java
@@ -37,5 +37,30 @@ class Scratch {
             }
         }
         return null;
+        /* 
+        BELOW IS THE WEIRD VERSION OF THAT THAT DOESN"T USE HASHSET:
+        public static int lonelyinteger(List<Integer> a) {
+    
+        Integer[] sortedArray = new Integer[a.size()];
+        sortedArray = a.toArray(sortedArray);
+        Arrays.sort(sortedArray);
+        System.out.println(Arrays.toString(sortedArray));
+        
+        int result = 0;
+        if (sortedArray.length == 1) {
+            return sortedArray[0];
+        } else if (sortedArray.length > 2 && sortedArray[0] != sortedArray[1]) {
+            return sortedArray[0];
+        } else if (sortedArray.length > 2 && sortedArray[sortedArray.length - 1] != sortedArray[sortedArray.length - 2]) {
+            return sortedArray[sortedArray.length - 1];
+        }
+        for (int i = 1; i < sortedArray.length - 1; i++) {
+            if (sortedArray[i] != sortedArray[i + 1] && sortedArray[i] != sortedArray[i - 1]) {
+                System.out.println(sortedArray[i] + ", " + sortedArray[i + 1] );
+                result = sortedArray[i];
+            }
+        }
+        return result;
+    }*/
     }
 }
