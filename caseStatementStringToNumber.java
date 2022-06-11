3fcb8fa8eecf6d6dad9e6c9de332b83de176f4bf
Lev <levdjepko@gmail.com>
2022-06-11 15:07:42 -0700

    Update caseStatementStringToNumber.java

--- caseStatementStringToNumber.java
+++ caseStatementStringToNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // start coding here
+        // get the string from the user input
         String number = scanner.next();
 
         switch (number) {
