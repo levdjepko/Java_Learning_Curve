751b4809e5748559cd83c0d87e0a420aab1dc9a0
Lev <levdjepko@gmail.com>
2023-02-06 09:15:30 -0800

    Update listSum.java

--- listSum.java
+++ listSum.java
@@ -1,1 +1,2 @@
-int sum = ar.stream().mapToInt(Integer::intValue).sum();
+int sum = ar.stream().mapToInt(Integer::intValue).sum(); // for ints
+long sum = ar.stream().mapToLong(Long::longValue).sum(); // for longs
