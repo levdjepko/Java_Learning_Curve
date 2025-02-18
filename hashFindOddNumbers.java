eafdd100ac8eeeae1715d3bf0d65172f35841231
Lev <levdjepko@gmail.com>
2025-02-18 10:06:18 -0800

    Update hashFindOddNumbers.java

--- hashFindOddNumbers.java
+++ hashFindOddNumbers.java
@@ -4,20 +4,20 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(int[] A) {
-        // write your code in Java SE 8
+        // hash sets in java
         HashMap<Integer, Integer> seen = new HashMap<>();
         for (int i = 0; i < A.length; i++) {
-            if(!seen.containsKey(A[i])) {
+            if (!seen.containsKey(A[i])) {
                 seen.put(A[i], 1);
             }
             else if (seen.containsKey(A[i])) {
-                if(seen.get(A[i]) == 1){
+                if (seen.get(A[i]) == 1){
                     seen.replace(A[i], 0);
                 }
             }
         }
         for (int i = 0; i < A.length; i++){
-            if(seen.get(A[i]) == 1){
+            if (seen.get(A[i]) == 1){
                 return A[i];
             }
         }
