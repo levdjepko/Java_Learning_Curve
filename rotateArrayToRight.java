db43c293aff52ef6c4ae9d346e3c746d41d198b3
Lev <levdjepko@gmail.com>
2025-03-23 08:18:08 -0700

    Update rotateArrayToRight.java

--- rotateArrayToRight.java
+++ rotateArrayToRight.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 class Scratch {
     public static void main(String[] args) {
-        int[] A = {1,2,3,4,5,6}; //INITIAL ARRAY
+        int[] A = {1,2,3,4,5,6}; // INITIAL ARRAY
         int K = 5; // ROTATE TO THE RIGHT HOW MANY TIMES (shift right)
 
         int[] newArr = new int[A.length];
@@ -16,7 +16,7 @@ class Scratch {
             System.out.println("New first index: " + newArr[0]);
             System.out.println(Arrays.toString(newArr));
             for (int index = A.length - 1; index > 0; index--){
-                //now we move the rest of elements one position to the right
+                // now, we move the rest of the elements one position to the right
                 newArr[index] = A[index - 1];
                 System.out.println("Index at " + index + " is: " + A[index-1]);
                 System.out.println(Arrays.toString(newArr));
